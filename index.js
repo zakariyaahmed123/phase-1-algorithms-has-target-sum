f1b88b90c9fa51d41334b60693d321660c224fd0
zakariyaahmed123 <118251771+zakariyaahmed123@users.noreply.github.com>
2022-12-29 21:44:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+for(i=0; i<array.length; i++){
+  let comp = array.splice(0, 1); 
+  if(array.includes(target-comp)){
+    debugger;
+    return true
+  }
+}return false}
 
 /* 
-  Write the Big O time complexity of your function here
+O(n)
 */
 
 /* 
   Add your pseudocode here
-*/
-
+should take an array and target number as arguments and return true if any sum of two array elements equals target number.
+should return true if the difference of an element in the array and the target number exist in the array.
+else should return false
 /*
-  Add written explanation of your solution here
+  function hasTargetSum accepts and array and a target as an argument and iterates over that array using a for loop.
+  The for loop used the splice method to desctuctively remove the first number in the array and save it to the comp variable.
+  The loop subtracts the spliced number from the target number to obtain the difference of those two numbers
+  Then using an includes method to determine if the difference of those two numbers exists in the array.
+  If so it returns true, if not it reiterates over the array of remaining numbers until it recieves a true statement in which case it returns true or reaches the end of the loop and returns false.
 */
 
 // You can run `node index.js` to view these console logs
@@ -32,3 +42,5 @@ if (require.main === module) {
 }
 
 module.exports = hasTargetSum;
+Footer
+
